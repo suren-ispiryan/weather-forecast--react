77b039799bc7b65976c35d77f5c951458f6a45ce
suren <suren.ispiryan@gmail.com>
2022-06-10 09:47:13 +0400

    fixing bugs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Link, Route } from 'react-router-dom';
 import WeatherForecast from './components/WeatherForecast';
 import FavoriteCityList from './components/FavoriteCityList';
